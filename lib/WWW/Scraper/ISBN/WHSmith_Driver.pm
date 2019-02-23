46dc83c9c61140241e3177ed247ca01f76375ea8
Barbie <barbie@missbarbell.co.uk>
2019-02-23 23:33:58 +0000

    upped version and copyright

--- lib/WWW/Scraper/ISBN/WHSmith_Driver.pm
+++ lib/WWW/Scraper/ISBN/WHSmith_Driver.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.08';
+$VERSION = '0.09';
 
 #--------------------------------------------------------------------------
 
@@ -236,7 +236,7 @@ be forthcoming, please feel free to (politely) remind me.
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2010-2015 Barbie for Miss Barbell Productions
+  Copyright (C) 2010-2019 Barbie for Miss Barbell Productions
 
   This distribution is free software; you can redistribute it and/or
   modify it under the Artistic Licence v2.
